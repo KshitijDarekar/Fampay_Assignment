3cf520b4937e159a30fae6a33ad85f9f4a8b0abb
Kshitij Darekar <kshitijdarekar13@gmail.com>
2022-06-29 17:44:02 +0530

    feat: result route
    ▷ /result route : get data from db and sort data in descending order of published time
    ▷ update readme file , for better installation and setup step
    ▷ update  pagination code logic
    ▷ remove incomplete code and add necessary comments

--- README.md
+++ README.md
@@ -11,77 +11,143 @@ To make an API to fetch latest videos sorted in reverse chronological order of t
 
 - Fetching the latest videos for a predefined search query and should store the data of vid eos (specifically these fields - Video title, description, publishing datetime, thumbnails URLs and any other fields you require)
 
+- A GET API which returns the stored video data in a paginated response sorted in descending order of published datetime.
+
 
 
 ## Tech Stack
 
-**Flask**
+**▷Flask**  
+**▷Flask-SQLAlchemy**  
+**▷SQLite**
 
 ## Environment Variables
 
-To run this project, you will need to add the following environment variables to your .env file.You can refer the samp.env file
+> To run this project, you will need to add the following environment variables to your .env file.You can refer the samp.env file.
 
-`YOUTUBE_API_KEY=`
+`YOUTUBE_API_KEY=`**enter your api key here without space**
 
 
-## Run Locally
+##Installation and  Run Locally
 
-Clone the project
+##### Clone the project
 
 ```bash
   git clone https://github.com/KshitijDarekar/Fampay_Assignment.git
 ```
 
-Go to the project directory
+##### Go to the project directory
 
 ```bash
-  cd <project-directory>
-```
-Setup virtual environment
-```
-$ python3 -m venv venv
+  cd Fampay_Assignment
 
 ```
-Activate venv
+##### Setup virtual environment
+###### macOS/Linux
 ```
-$ . venv/bin/activate
+python3 -m venv venv
 
 ```
-Install Flask
+###### Windows
+```
+py -3 -m venv venv
+
+```
+
+##### Activate venv
+###### macOS/Linux
+
+```
+ . venv/bin/activate
+
+```
+###### Windows
+```
+ venv\Scripts\activate
+```
+##### Install Flask
 ```
 $ pip install Flask
 
 ```
-Install dependencies
-
+##### Install dependencies
+###### (for python 2.x)
 ```bash
-$ pip install -r requirements.txt  (for python 2.x)
-
+$ pip install -r requirements.txt  
+```
+###### (for python 3.x)
+```bash
 $ pip3 install -r requirements.txt (for python 3.x)
 ```
 
-Set flaskenv and 
+##### Set flaskenv
+> Run the following commands based on the terminal you are using
+
+###### Bash
 ```bash
-$ export FLASK_APP=server
-$ export FLASK_ENV=development
-
-
+export FLASK_APP=server
+export FLASK_ENV=development
 ```
-Run Flask server
+###### CMD
+```bash
+set FLASK_APP=server
+set FLASK_ENV=development
+```
+###### PowerShell
+```bash
+$env:FLASK_APP = "server"
+$env:FLASK_ENV = "development"
+```
+
+##### Run Flask server
 ```
 $ flask run
 ```
-Check the route /paginate to view paginated api
-Check
+##### Setup Database
+> ###### Open new terminal in the same directory and run the following lines in the command line
 
-## Routes
+```
+$ flask shell
+```
+###### Now Inside the flask shell to create the  database model
+```
+>>> from server import db
+>>> db.create_all()
 
-```javascript
-localhost:5000/paginate
-returns paginated response
 ```
-```
-localhost:5000/videos
-list the details of fetched videos based on a particular search query
-```
+
+
+## Routes / API Reference
+
+| Route  | Desc  | Method  | Note  |
+| ------------ | ------------ | ------------ | ------------ |
+|  `/search` | search route to list all the video data related to a specific search query and store the data in the Database  |  GET |go to this route before going to the `/result` as this route stores the data in the db   |
+|  `/result` | returns the **stored video data** from DB in a **paginated response** sorted in **descending order** of published datetime   |  GET | This is the expected result  |
+|  `/videos` | returns video data based on specific search query directly from the Youtube Api  |  GET |   |
+|  `/paginate` | returns the video data (**directly from the Youtube API**) in a         **paginated response** sorted in descending order of published datetime. |  GET |   |
+|  `/db` |  returns the video data from the data base sorted in descending order of published datetime **without pagination** |   GET|  returns data from db |
+
+                
+----
+
+## Acknowledgements / References
+
+- https://help.parsehub.com/hc/en-us/articles/217751808-API-Tutorial-How-to-get-run-data-using-Python-Flask
+
+- https://pypi.org/project/requests/
+
+- https://stackoverflow.com/questions/44138848/make-api-request-every-x-seconds-in-python-3
+
+- https://flask-restless.readthedocs.io/en/latest/sorting.html
+
+- https://stackoverflow.com/questions/55543011/flask-restful-pagination
+
+- https://www.digitalocean.com/community/tutorials/how-to-use-flask-sqlalchemy-to-interact-with-databases-in-a-flask-application
+
+- https://flask.palletsprojects.com/en/2.1.x/patterns/sqlalchemy/
+- YouTube data v3 API: [https://developers.google.com/youtube/v3/getting-started](https://developers.google.com/youtube/v3/getting-started)
+- Search API reference: [https://developers.google.com/youtube/v3/docs/search/list](https://developers.google.com/youtube/v3/docs/search/list)
+
+----
+
 
--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 from flask import Flask,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import exists    
-
 import grequests
 import requests
 import json
 from flask import request
+from dateutil import parser
+from datetime import datetime
 # from flask_paginate import Pagination, get_page_args
 from apscheduler.schedulers.background import BackgroundScheduler as scheduler
 from celery import Celery
@@ -16,48 +17,47 @@ API_KEY = os.environ.get('YOUTUBE_API_KEY')
 basedir = os.path.abspath(os.path.dirname('./'))
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
-# print("base dir =  ",os.basedir)
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 db = SQLAlchemy(app)
-# app.config['CELERY_BROKER_URL']     = 'redis://localhost:6379/0'
-# app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-# celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-# celery.conf.update(app.config)
 
+"""
+==============
+Model
+==============
+"""
+# Video Model Schema
 class Video(db.Model):
     id = db.Column(db.String(20), primary_key=True)
     title = db.Column(db.String(50))
     description = db.Column(db.String(120))
     url = db.Column(db.String(50))
-    publishedAt=db.Column(db.String(20))
+    publishedAt=db.Column(db.DateTime(20))
 
     def __repr__(self):
         return '<Video %r>' % self.title
 
-# db.create_all()
 
-# @app.route
-# @celery.task
-# def my_background_task(arg1, arg2):
-#     # some long running task here
-#     return "Hello , I am running asynchrounously"
-
-# task = my_background_task.delay(10, 20)
-
-# task = my_background_task.apply_async(args=[10, 20], countdown=30)
-
-
+"""
+==============
+Routes
+==============
+"""
+# @route   GET /
+# @access  Public
 @app.route("/")
 def hello_world():
     return "<p>Server is Running !</p>"
 
-# search route to list all the video data related to a specific search query
+# @desc  - search route to list all the video data related to a specific search query and store the data in the Database
+#        - In the browser , go to this route before going to the `/result` as this route stores the data in the db
+# @route   GET /search
+# @access  Public
 @app.route("/search")
 def search():
+    
     s_params={
         'part':'snippet',
         'maxResults':'10',
@@ -73,9 +73,7 @@ def search():
     
     for i in result:
         video_list.append(i['id']['videoId'])
-         
-    print(video_list)
-    
+             
     video_url = 'https://youtube.googleapis.com/youtube/v3/videos'
     v_params = {
         'part':'snippet',
@@ -85,15 +83,20 @@ def search():
 
     request_video = requests.get(video_url,params=v_params)
     video_data=request_video.json()
-    video_limited_data={}
+    video_specific_data={}
+
     for data in video_data['items']:
         current_data=[]
 
         current_data.append(data['snippet']['title'])
         current_data.append(data['snippet']['description'])
         current_data.append(data['snippet']['thumbnails']['default']['url'])
-        current_data.append(data['snippet']['publishedAt'])
-        
+        # format dateTime from type String to type DateTime
+        date = data['snippet']['publishedAt'].replace("T", " ")
+        date_time = date.replace("Z", "")
+        formatted_date=datetime.fromisoformat(date_time)
+        # Append data to the current_data list
+        current_data.append(formatted_date)
         current_data.append({
             'title':data['snippet']['title'],
             'description':data['snippet']['description'],
@@ -103,36 +106,40 @@ def search():
         
         # exists = db.session.query(db.exists().where(Video.id == data['id'])).scalar()
         exists=bool(Video.query.filter_by(id=data['id']).first())
-        print("exists: \n",exists)
         #Check if the data already exists in the DB
         if(exists!=True):
-            #Add data to DB 
+            #If not exists then Add data to DB 
             db_data = Video(
             id=data['id'],
             title=data['snippet']['title'],
             description=data['snippet']['description'],
             url=data['snippet']['thumbnails']['default']['url'],
-            publishedAt=data['snippet']['publishedAt']
+            publishedAt=formatted_date
             )
 
             db.session.add(db_data)
             db.session.commit()    
 
-
-
-                
-        video_limited_data['items']=current_data
-        
-        print("data=",data['snippet']['title'],data['snippet']['description'],data['snippet']['thumbnails']['default']['url'],data['snippet']['publishedAt'])
-        print(json.dumps(current_data,indent=4,sort_keys=True))
-        
-    
-    # print(json.dumps(video_data, indent=4, sort_keys=True))
-    # return video_data
-    print(json.dumps(video_limited_data, indent=4, sort_keys=True))
+        video_specific_data['items']=current_data
     return video_data
 
-# returns list of video api with required data based on search query
+# returns the stored video data from DB in a paginated response sorted in descending order of published datetime .
+# @route   GET /result
+# @access  Public
+@app.route("/result",methods=['GET'])
+def result():
+
+    return jsonify(get_paginated_list_db(
+		Video, 
+		'/result', 
+		start=request.args.get('start', 1), 
+		limit=request.args.get('limit', 3)
+	))
+
+
+# returns video data based on specific search query directly from the Youtube Api
+# @route   GET /videos
+# @access  Public
 @app.route("/videos")
 def videos():
     s_params={
@@ -151,10 +158,13 @@ def videos():
         'key':API_KEY
     }
     
-    video_limited_data = get_video_list(video_url, v_params)
-    return video_limited_data
+    video_specific_data = get_video_list(video_url, v_params)
+    return jsonify(video_specific_data)
 
 
+# @desc    A GET API which returns the video data (directly from the Youtube API) in a         paginated response sorted in descending order of published datetime.
+# @route   GET /paginate
+# @access  Public
 @app.route("/paginate",methods=['GET'])
 def paginate():
     
@@ -177,35 +187,67 @@ def paginate():
         'id':video_list,
         'key':API_KEY
     }
-#['eECgSJis8jM', 'QXhV148EryQ', 'xnqCrnYBz2U', 'Cf3vKcxmc-A', '8_sq4qcf20I', 'kWE1mHBATj0', 'LEc7W2C6kcM', 'vTVuB1kgpd4', 'HL8Er3cP2b4', '1p0yFxZhKfo']
-    video_limited_data = get_video_list(video_url, v_params)
-    print(json.dumps(video_limited_data))
-    # print(json.dumps(video_data, indent=4, sort_keys=True))
-    # return video_data
-    print(json.dumps(video_limited_data, indent=4, sort_keys=True))
+
+    video_specific_data = get_video_list(video_url, v_params)
     
     # paginate the data and store in result
-    print("typeof current_data =" ,type(video_limited_data))
-    print("typeof json.dumps(current_data) =" ,type(json.dumps(video_limited_data)))
-    
-    result= get_paginated_list(video_limited_data, 'localhost:5000/paginate', '2', '3')
-    print("result=",result)
-    return result
 
+    result= get_paginated_list(
+        video_specific_data,
+        '/paginate',
+        start=request.args.get('start', 1), 
+		limit=request.args.get('limit', 20)
+        )
+    return jsonify(result)
+
+# @desc    returns the video data from the data base in sorted in descending order of published datetime without pagination
+# @route   GET /db
+# @access  Public
 @app.route("/db")
 def get_db_data():
-    data = Video.query.all()
-    print("data = \n",json.dumps(data))
-    return "Hello"
+    
+    videos = Video.query.order_by(Video.publishedAt.desc()).all()
+    video_data={}
+    current_data=[]
+
+    for video in videos:
+        current_data.append({
+            'id':video.id,
+            'title':video.title,
+            'description':video.description,
+            'url':video.url,
+            'publishedAt':video.publishedAt
+        })
+    video_data["items"]=current_data
+    return jsonify(video_data)
         
     
 
+
+"""
+==============
+Functions
+==============
+"""
+
 # Function to paginate List
 def get_paginated_list(results, url, start, limit):
+    """Paginates the video data.
+
+    Parameters
+    ----------
+    results: list
+    url : String
+    start : String
+    limit : String
+
+    Returns
+    -------
+    [paginated json object]
+    """
     start = int(start)
     limit = int(limit)
     count = len(results)
-    print("count",count)
     if count < start or limit < 0:
         abort(404)
     # make response
@@ -229,35 +271,50 @@ def get_paginated_list(results, url, start, limit):
         obj['next'] = url + '?start=%d&limit=%d' % (start_copy, limit)
     # finally extract result according to bounds
     obj['results'] = results[(start - 1):(start - 1 + limit)]
-    print("type of results: ",type(results))
-    print("Result==",json.dumps(results[1:11]))
-    
-    print("Obj==",json.dumps(obj))
+
     return obj
 
 # Function to get search query result from api
 def get_search_list(url,s_params):
-    
+    """Data based on a predined search query
+
+    Parameters
+    ----------
+    url:String
+    s_params : Dictionary
+
+    Returns
+    -------
+    [List of video ids of videos for a predefined search query]
+    """
     video_list = []
     
     r=requests.get('https://youtube.googleapis.com/youtube/v3/search',params=s_params)
     data=r.json()
     result = data["items"]
-    
+    #Iterate result to store videoId in a list
     for i in result:
         video_list.append(i['id']['videoId'])
          
-    print("search list function : ",video_list)
     return video_list
         
 # Function to get video data for a particular search query from api
 def get_video_list(url,v_params):
-    
+    """Video Data based on predefined search query
+
+    Parameters
+    ----------
+    url:String
+    s_params : Dictionary
+
+    Returns
+    -------
+    [list of Video Data for the given video ids (params)]
+    """
     request_video = requests.get(url,v_params)
-    
+    # Serialize the data
     video_data=request_video.json()
-    # print("raw data =",video_data)
-    video_limited_data={}
+    video_specific_data={}
     # create an array to store in the items attribute
     current_data=[]
 
@@ -269,14 +326,77 @@ def get_video_list(url,v_params):
             'url':data['snippet']['thumbnails']['default']['url'],
             'publishedAt':data['snippet']['publishedAt']
         })
-                
-    print("Current DATA : ", json.dumps(current_data,indent=4))           
-    video_limited_data["items"]=current_data
-    print("current data 1st element:",json.dumps(current_data[0],indent=4))
+                        
+    video_specific_data["items"]=current_data
     return current_data
 
+# Function to paginate List , a little different from get_paginated_list()
+def get_paginated_list_db(model, url, start, limit):
+    """Function returns the stored video data in a paginated response sorted in descending order of published datetime.
 
-# Run Scheduler every 10 sec
+    Parameters
+    ----------
+    results: list
+    url : String
+    start : String
+    limit : String
+
+    Returns
+    -------
+    [stored video data in a paginated response sorted in descending order of published datetime.]
+    Note:
+    ------
+    get_paginated_list() function also perform the same task , the only difference is in parameters , in this function then `model` parameter is passed which is a DB model. While in the case of get_paginated_list() the parameter is a list object .
+    
+    Kept both the functions here for understanding purpose .
+    """
+    
+    results = model.query.order_by(Video.publishedAt.desc()).all()
+    start = int(start)
+    limit = int(limit)
+    count = len(results)
+    
+    #store data from the data base to a list to avoid following error
+    #Error : Object of type Video is not JSON serializable
+    current_data=[]
+
+    for video in results:
+        current_data.append({
+            'id':video.id,
+            'title':video.title,
+            'description':video.description,
+            'url':video.url,
+            'publishedAt':video.publishedAt
+        })
+    if (count < start):
+        abort(404)
+    # make response
+    obj = {}
+    obj['start'] = start
+    obj['limit'] = limit
+    obj['count'] = count
+    # make URLs
+    # make previous url
+    if start == 1:
+        obj['previous'] = ''
+    else:
+        start_copy = max(1, start - limit)
+        limit_copy = start - 1
+        obj['previous'] = url + '?start=%d&limit=%d' % (start_copy, limit_copy)
+    # make next url
+    if start + limit > count:
+        obj['next'] = ''
+    else:
+        start_copy = start + limit
+        obj['next'] = url + '?start=%d&limit=%d' % (start_copy, limit)
+    # finally extract result according to bounds
+    obj['results'] = current_data[(start - 1):(start - 1 + limit)]
+    return obj
+
+
+
+# # This Code runs a particular function in the background after a set interval
+# # Run Scheduler every 10 sec
 # sch = scheduler()
 # sch.add_job(paginate, 'interval', seconds=10)
 # sch.start()
